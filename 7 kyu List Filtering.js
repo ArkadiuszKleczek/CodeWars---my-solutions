e47dc15c5dc21aeb5f84add2874d21281442793f
Arkadiusz Kłeczek <54043251+ArkadiuszKleczek@users.noreply.github.com>
2022-03-19 10:47:20 +0100

    Add files via upload

---
+++ 7 kyu List Filtering.js
@@ -0,0 +1,6 @@
+/*
+*   7 kyu List Filtering
+*   https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/solutions/javascript/me/best_practice
+* */
+
+const filter_list = (l) => l.filter(e => Number.isInteger(e))
