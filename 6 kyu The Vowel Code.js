062620047c3d4be984889bcac83332f8243d82ba
Arkadiusz Kłeczek <54043251+ArkadiuszKleczek@users.noreply.github.com>
2022-04-01 11:29:11 +0200

    Add files via upload

---
+++ 6 kyu The Vowel Code.js
@@ -0,0 +1,29 @@
+function encode(string) {
+    const strArr = string.split('');
+    let encoded = []
+    for (let i = 0; i < strArr.length; i++){
+        if (strArr[i] === "a"){encoded.push("1")}
+        else if (strArr[i] === "e"){encoded.push("2")}
+        else if (strArr[i] === "i"){encoded.push("3")}
+        else if (strArr[i] === "o"){encoded.push("4")}
+        else if (strArr[i] === "u"){encoded.push("5")}
+        else {encoded.push(strArr[i])}
+    }
+    console.log(encoded.join(""));
+    return encoded.join("")
+}
+
+function decode(string) {
+    const strArr = string.split('');
+    let decoded = []
+    for (let i = 0; i < strArr.length; i++){
+        if (strArr[i] === "1"){decoded.push("a")}
+        else if (strArr[i] === "2"){decoded.push("e")}
+        else if (strArr[i] === "3"){decoded.push("i")}
+        else if (strArr[i] === "4"){decoded.push("o")}
+        else if (strArr[i] === "5"){decoded.push("u")}
+        else {decoded.push(strArr[i])}
+    }
+    console.log(decoded.join(""));
+    return decoded.join("")
+}
