f58d7ac3e9e36e9fa97ec29942e296032729bea9
Arkadiusz Kłeczek <54043251+ArkadiuszKleczek@users.noreply.github.com>
2022-04-16 10:50:01 +0200

    Add files via upload

---
+++ 5 kyu Part 1 - Evil Programming Government Restrictions.js
@@ -0,0 +1,12 @@
+/*
+* 5 kyu Part 1: Evil Programming Government Restrictions
+*   https://www.codewars.com/kata/5255892719453db6d2000a23
+* */
+
+const anarchy = () => {
+    const one = true + false;
+    const two = one + true;
+    const three = two + true;
+    return `I can write numbers like, ${one}, ${two}, ${three}.`
+}
+
