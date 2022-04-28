76d07c07c4ac52572d5d415063f9cbd3ca26fb3b
Arkadiusz Kłeczek <54043251+ArkadiuszKleczek@users.noreply.github.com>
2022-04-28 23:49:09 +0200

    Add files via upload

---
+++ 6 kyu Encrypt this.js
@@ -0,0 +1,12 @@
+/*
+*   6 kyu Encrypt this
+*   https://www.codewars.com/kata/5848565e273af816fb000449
+* */
+
+const encryptThis = text => {
+    const textArr = text.split(' ');
+    for (let i = 0; i < textArr.length; i++) {
+        textArr[i].split('');
+
+    }
+}
