502a5db25d5325836dec417b9341b22efb238ca7
Arkadiusz Kłeczek <54043251+ArkadiuszKleczek@users.noreply.github.com>
2022-05-20 17:16:00 +0200

    Add files via upload

---
+++ 6 kyu Array Helpers.js
@@ -0,0 +1,28 @@
+/*
+*   6 kyu Array Helpers
+*   https://www.codewars.com/kata/525d50d2037b7acd6e000534
+* */
+
+Array.prototype.square = function() {
+    return this.map(n => n ** 2);
+};
+
+Array.prototype.cube = function() {
+    return this.map(n => n ** 3);
+};
+
+Array.prototype.sum = function() {
+    return this.reduce((a, b) => a + b, 0);
+};
+
+Array.prototype.average = function() {
+    return this.sum() / this.length;
+};
+
+Array.prototype.even = function() {
+    return this.filter(n => n % 2 === 0);
+};
+
+Array.prototype.odd = function() {
+    return this.filter(n => n % 2 !== 0);
+};
